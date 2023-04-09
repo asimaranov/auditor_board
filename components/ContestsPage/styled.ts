72f5472f2859e675a4d4efd285ca6c8293135cac
asimaranov <a.simaranov@gmail.com>
2023-04-09 18:31:07 +0300

    Fixed mobile support

--- components/ContestsPage/styled.ts
+++ components/ContestsPage/styled.ts
@@ -10,7 +10,7 @@ display: flex;
 
 flex-direction: row;
 
-  @media (max-width: 800px) {
+  @media screen and (max-width: 800px) {
     & {
       flex-direction: column;
       align-items: center;
