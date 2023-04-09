96e9057b89aa0e3ed6f519d0bdd4138aa792faad
asimaranov <a.simaranov@gmail.com>
2023-04-09 21:12:13 +0300

    Fixed mobile support

--- components/ScoreBoard/index.tsx
+++ components/ScoreBoard/index.tsx
@@ -8,18 +8,19 @@ import { AuditorLink } from 'components/AuditorLink.tsx';
 export const ScoreBoard = ({ extended, title }: { extended: boolean, title: string }) => {
     const [auditorResults, setAuditorResults] = useAuditorsResults();
     const [competitionIds, setCompetitionIds] = useCompetitionIds();
-
+    const competitionsNum = Math.min(extended ? (window.innerWidth > 800 ? 7 : 3) : 0, competitionIds.length);
+    
     return (
         <ScoreBoardContainer>
             <p>{title}</p>
             <SbtInfoUpdater />
-            <ScoreBoardTable competitionsNum={extended && competitionIds?.length || 0}>
+            <ScoreBoardTable competitionsNum={competitionsNum}>
                 <thead style={{ display: 'contents' }}>
                     <ScoreBoardHeader>
                         <ScoreBoardHeaderCell>#</ScoreBoardHeaderCell>
                         <ScoreBoardHeaderCell>Address</ScoreBoardHeaderCell>
                         <ScoreBoardHeaderCell>Socials</ScoreBoardHeaderCell>
-                        {extended && competitionIds?.map(x => (<ScoreBoardHeaderCell>{x}</ScoreBoardHeaderCell>))}
+                        {extended && competitionIds?.slice(-competitionsNum).map(x => (<ScoreBoardHeaderCell>{x}</ScoreBoardHeaderCell>))}
                         <ScoreBoardHeaderCell>Total</ScoreBoardHeaderCell>
                     </ScoreBoardHeader>
                 </thead>
@@ -29,7 +30,7 @@ export const ScoreBoard = ({ extended, title }: { extended: boolean, title: stri
                         <ScoreBoardCell><AuditorLink address={x.address} /></ScoreBoardCell>
                         <ScoreBoardCell>{<div>N/A</div>}</ScoreBoardCell>
 
-                        {extended && competitionIds?.map((y, i) => {
+                        {extended && competitionIds?.slice(-competitionsNum).map((y, i) => {
                             const foundUserCompetition = x.competitions.find(e => e.id == y);
                             return (
                                 <ScoreBoardCell key={i}>
