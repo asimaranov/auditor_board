d83ff46eba48186c86a7b1c3696f037dc7c041f8
asimaranov <a.simaranov@gmail.com>
2023-04-16 12:00:25 +0300

    Remove auditors section

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
     <NavbarContainer>
         <NavbarSection active={pathname == '/'}><Link href="/">Main</Link></NavbarSection>
         <NavbarSection active={pathname == '/contests'}><Link href="/contests">Contests</Link></NavbarSection>
-        <NavbarSection active={pathname == '/auditors'}><Link href="/auditors">Auditors</Link></NavbarSection>
+        {/* <NavbarSection active={pathname == '/auditors'}><Link href="/auditors">Auditors</Link></NavbarSection> */}
         <NavbarSection active={pathname == '/profile'}><Link href="/profile">Profile</Link></NavbarSection>
         
         <ConnectWallet><Web3Button /></ConnectWallet>
