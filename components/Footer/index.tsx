78be55c7e023e0adfd3f2239b92cd7b1795f36c0
asimaranov <a.simaranov@gmail.com>
2023-08-31 10:51:37 +0300

    Added star button

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,7 +5,10 @@ export const Footer = () => {
     return (
         <div>
             <FooterContainer>
-                <a href={'https://strongholdsec.io/'} >Request audit</a> | <a href={'https://github.com/asimaranov/auditor_board'} >Github</a> | <a href={'https://twitter.com/stronghold_dao'} >Twitter</a>
+                <a href={'https://strongholdsec.io/'} >Request audit</a> | 
+                <a href={'https://github.com/asimaranov/auditor_board'} >Github</a> | 
+                <a href={'https://twitter.com/stronghold_dao'} >Twitter</a> | 
+                <iframe src="https://ghbtns.com/github-btn.html?user=asimaranov&repo=auditor_board&type=star&count=true" frameBorder="0" scrolling="0" width="150" height="20" title="GitHub"></iframe>
             </FooterContainer>
         </div>
     )
